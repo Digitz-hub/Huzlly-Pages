fad5edca5edbc05ddb474433c3eff973b84f56c6
Digitz-hub <itskartik2001@gmail.com>
2026-09-02 23:38:35 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://huzlly-pages.pages.dev',
+  site: 'https://huzlly.com',
   integrations: [
     sitemap(),
     tailwind({
